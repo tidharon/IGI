15d365698d3c52bbd4765db997396f527ec28a06
TidyUSA <tidhar.sharon10@gmail.com>
2020-04-02 16:36:29 +0300

    added problem submit + solution submit and choose

--- app/src/main/java/com/example/igi/Problem.java
+++ app/src/main/java/com/example/igi/Problem.java
@@ -9,26 +9,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class Problem extends AppCompatActivity implements View.OnClickListener {
-Button butPhotoPrblm;
-Button butRecPrblm;
-Button butInfo;
+    Button butPhotoPrblm;
+    Button butRecPrblm;
+    Button butInfo, butSubmit;
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem);
 
-        butPhotoPrblm = (Button)findViewById(R.id.butPhotoPrblm);
+        butPhotoPrblm = (Button) findViewById(R.id.butPhotoPrblm);
         butPhotoPrblm.setOnClickListener(this);
-        butRecPrblm = (Button)findViewById(R.id.butRecPrblm);
+        butRecPrblm = (Button) findViewById(R.id.butRecPrblm);
         butRecPrblm.setOnClickListener(this);
-butInfo = (Button)findViewById(R.id.info_but);
-butInfo.setOnClickListener(this);
+        butInfo = (Button) findViewById(R.id.info_but);
+        butInfo.setOnClickListener(this);
+        butSubmit = (Button) findViewById(R.id.submit_but);
+        butSubmit.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v==butInfo) {
+        if (v == butInfo) {
             Intent intentDiscover = new Intent(this, InfoPage.class);
             startActivity(intentDiscover);
             finish();
--- app/src/main/java/com/example/igi/Solution.java
+++ app/src/main/java/com/example/igi/Solution.java
@@ -4,31 +4,44 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Solution extends AppCompatActivity implements View.OnClickListener {
-Button butPhotoSltn;
-Button butRecSltn;
-Button butInfo;
+    Button butPhotoSltn;
+    Button butRecSltn;
+    Button butInfo;
+    Button butSubmit;
+    Spinner listProblem;
+    ArrayAdapter<String> problemArray;
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solution);
 
-        butPhotoSltn = (Button)findViewById(R.id.butPhotoSltn);
+        butPhotoSltn = (Button) findViewById(R.id.butPhotoSltn);
         butPhotoSltn.setOnClickListener(this);
-        butRecSltn = (Button)findViewById(R.id.butRecSltn);
+        butRecSltn = (Button) findViewById(R.id.butRecSltn);
         butRecSltn.setOnClickListener(this);
-butInfo = (Button)findViewById(R.id.info_but);
-butInfo.setOnClickListener(this);
+        butInfo = (Button) findViewById(R.id.info_but);
+        butInfo.setOnClickListener(this);
+        butSubmit = (Button) findViewById(R.id.submit_but);
+        butInfo.setOnClickListener(this);
+        listProblem = (Spinner) findViewById(R.id.problemList);
+        problemArray = new ArrayAdapter<>(Solution.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.problemList));
+        problemArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        listProblem.setAdapter(problemArray);
     }
 
     @Override
     public void onClick(View v) {
-        if (v==butInfo) {
+        if (v == butInfo) {
             Intent intentDiscover = new Intent(this, InfoPage.class);
             startActivity(intentDiscover);
             finish();
