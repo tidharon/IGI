8850b776edb7db3ff15578cf3cbdb507ea129d34
TidyUSA <tidhar.sharon10@gmail.com>
2020-04-06 22:19:10 +0300

    added toasts to dev and rate

--- app/src/main/java/com/example/igi/Develop.java
+++ app/src/main/java/com/example/igi/Develop.java
@@ -6,21 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import static com.example.igi.R.*;
 
 public class Develop extends AppCompatActivity implements View.OnClickListener {
-Button butPhotoDvlp;
-Button butRecDvlp;
-ImageView butInfo;
+    Button butSubmit;
+    ImageView butInfo;
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_develop);
-
+        butSubmit = (Button) findViewById(id.dvlpSubmitBut);
+        butSubmit.setOnClickListener(this);
         butInfo = (ImageView) findViewById(id.info_but);
         butInfo.setOnClickListener(this);
 
@@ -29,9 +31,12 @@ ImageView butInfo;
 
     @Override
     public void onClick(View v) {
-        if (v==butInfo) {
+        if (v == butInfo) {
             Intent intentDiscover = new Intent(this, InfoPage.class);
             startActivity(intentDiscover);
+        }
+        if (v == butSubmit) {
+            Toast.makeText(getApplicationContext(), "Thank You For Helping!", Toast.LENGTH_SHORT).show();
         }
 
     }
--- app/src/main/java/com/example/igi/Rate.java
+++ app/src/main/java/com/example/igi/Rate.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Rate extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
     ImageView butInfo;
+    Button butVote;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -20,6 +22,8 @@ public class Rate extends AppCompatActivity implements View.OnClickListener, Rad
         setContentView(R.layout.activity_rate);
         butInfo = (ImageView) findViewById(R.id.info_but);
         butInfo.setOnClickListener(this);
+        butVote = (Button)findViewById(R.id.voteSltnBut);
+        butVote.setOnClickListener(this);
 
     }
 
@@ -28,6 +32,9 @@ public class Rate extends AppCompatActivity implements View.OnClickListener, Rad
         if (v == butInfo) {
             Intent intentDiscover = new Intent(this, InfoPage.class);
             startActivity(intentDiscover);
+        }
+        if (v==butVote){
+            Toast.makeText(getApplicationContext(), "Every Vote Affects. Thanks!", Toast.LENGTH_SHORT).show();
         }
     }
 
