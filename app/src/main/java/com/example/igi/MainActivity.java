2bcd96f568ca512394e1e0ce3112b2475b043bf6
TidyUSA <tidhar.sharon10@gmail.com>
2020-04-22 20:49:53 +0300

    added signup process

--- app/src/main/java/com/example/igi/MainActivity.java
+++ app/src/main/java/com/example/igi/MainActivity.java
@@ -7,10 +7,15 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -22,7 +27,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button butToSignUp;
     Button butInfo;
     String Tag = "IGI";
-
+    EditText editUsename, editPassword;
+    ProgressBar PBLogin;
+    FirebaseAuth fAuth;
+    String name, password;
 
     /**
      * This function requests premissions
@@ -90,12 +98,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butLogin.setOnClickListener(this);
         butToSignUp.setOnClickListener(this);
         butInfo.setOnClickListener(this);
+        PBLogin = findViewById(R.id.Login_progressBar);
+        fAuth = FirebaseAuth.getInstance();
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), HomeScreen.class));
+        }
 
     }
 
     @Override
     public void onClick(View v) {
         if (v == butLogin) {
+
+            PBLogin.setVisibility(View.VISIBLE);
+
+            name = editUsename.getText().toString().trim();
+            password = editPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(name)) {
+                editUsename.setError("Please enter Your Username");
+            }
+            if (TextUtils.isEmpty(password)) {
+                editPassword.setError("please enter password");
+            }
+
             Intent intentLogin = new Intent(this, HomeScreen.class);
             startActivity(intentLogin);
             finish();
--- app/src/main/java/com/example/igi/signup.java
+++ app/src/main/java/com/example/igi/signup.java
@@ -1,18 +1,32 @@
 package com.example.igi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity implements OnClickListener {
     Button butSignUp;
     ImageView butInfo;
+    EditText editEmail, editName, editPassword;
+    FirebaseAuth fAuth;
+    ProgressBar PBSignup;
+    String email, name, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +37,51 @@ public class signup extends AppCompatActivity implements OnClickListener {
         butSignUp.setOnClickListener(this);
         butInfo = (ImageView) findViewById(R.id.info_but);
         butInfo.setOnClickListener(this);
+        editEmail = findViewById(R.id.fldEmail);
+        editName = findViewById(R.id.fldUsename);
+        editPassword = findViewById(R.id.fldPassword);
+        PBSignup = findViewById(R.id.Signup_progressBar);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser()!= null){
+            startActivity(new Intent(getApplicationContext(), HomeScreen.class));
+        }
     }
 
     public void onClick(View v) {
         if (v == butSignUp) {
-            Intent intentHomeScreen = new Intent(this, HomeScreen.class);
-            startActivity(intentHomeScreen);
-            finish();
+            PBSignup.setVisibility(View.VISIBLE);
+
+            email = editEmail.getText().toString().trim();
+            name = editName.getText().toString().trim();
+            password = editPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                editEmail.setError("Please Enter Email");
+            }
+            if (TextUtils.isEmpty(name)) {
+                editName.setError("Please enter Your Username");
+            }
+            if (TextUtils.isEmpty(password)) {
+                editPassword.setError("please enter password");
+            }
+
+            fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(signup.this, "Welecome!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), HomeScreen.class));
+                        finish();
+                    } else {
+                        Toast.makeText(signup.this, "ERROR! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+
+
         }
         if (v == butInfo) {
             Intent intentDiscover = new Intent(this, InfoPage.class);
